475ae8df7a62b64859911d10577ecc5fca748d09
Matheus Oliveira da Silva <oliveiradsmatheus@gmail.com>
2024-09-07 13:21:51 -0300

    Implementação do Protótipo de execução do código py

--- Interpretador.c
+++ Interpretador.c
@@ -9,7 +9,7 @@
 #include "Headers\\Moldura.h"
 #include "Headers\\FuncoesPy.h"
 
-void ExibirPrograma(FILE *arq, int *LinhaAtual) {
+void ExibirPrograma(FILE *arq, int LinhaAtual) {
 	char linha[100];
 	int l=10, c=34;
 
@@ -17,10 +17,11 @@ void ExibirPrograma(FILE *arq, int *LinhaAtual) {
 
 	fgets(linha,100,arq);
 	while(!feof(arq)) {
-		if(l==(*LinhaAtual+9)) {
+		if(l==(LinhaAtual+10)) {
 			textbackground(14);
 			textcolor(0);
 		}
+		LimpaLinha(l);
 		gotoxy(c,l);
 		printf("%s",linha);
 		textbackground(0);
@@ -57,23 +58,70 @@ int ContLinha (FILE *arq) {
 	return tam;
 }
 
+int ProximaLinha (char token[30], FILE *arq, int Linha) {
+	char linha[100], tokenArq[30];
+	int i=0,j;
+
+	rewind(arq);
+
+	//Encontrando a linha atual dentro do arquivo
+	fgets(linha,100,arq);
+	while(!feof(arq) && i<Linha) {
+		fgets(linha,100,arq);
+		i++;
+	}
+	
+	//Encontrando a primeira LETRA da linha
+	i=0;
+	while(i<strlen(linha) && !((linha[i] > 64 && linha[i] < 91) || (linha[i] > 96 && linha[i] < 123)))
+		i++;
+	j=0;
+	//Armazenando o primeiro token para comparacao
+	while ((i<strlen(linha)) && ((linha[i] > 47 && linha[i] < 58) || (((linha[i] > 64 && linha[i] < 91) || (linha[i] > 96 && linha[i] < 123)))))
+		tokenArq[j++] = linha[i++];
+	tokenArq[j] = '\0';
+
+	LimpaMsg();
+	EscrMsg("");
+	printf("PTR: %s // ARQ: %s",token,tokenArq);
+
+	if(strcmp(token,tokenArq)==0)
+		return Linha;
+	return ProximaLinha(token,arq,Linha+1);
+}
+
 void ExecPassos (List *L, FILE *arq) {
 	char op;
-	int LinhaAtual=1, qtdeL;
+	int LinhaAtual=0, qtdeL;
 
 	qtdeL = ContLinha(arq);
 	LimpaTela();
 	ExibirTexto((char*)"EXECUCAO DO PROGRAMA:",34,8,14);
 
 	do {
-		ExibirPrograma(arq,&LinhaAtual);
+		ExibirPrograma(arq,LinhaAtual);
+		LimpaMsg();
+		EscrMsg("");
+		printf("%d", LinhaAtual);
+		printf("EXECUTANDO %s", L->pToken->tokenName);
+
+		// Executa Linha de Código
+
 		op = getch();
 
 		switch(op) {
-			case 13:
-				LinhaAtual++;
-				if(LinhaAtual>qtdeL)
-					op = 27;
+			case 13: // Andando nas linhas do programa
+			if(L->prox) {
+				L = L->prox;
+				// Tokens que não são Linhas de Código.
+				if(strcmp(L->pToken->tokenName,"fim")==0)
+					L = L->prox;
+				if(L)
+					LinhaAtual = ProximaLinha(L->pToken->tokenName,arq,LinhaAtual+1);
+				
+			}
+			else
+				op=27;
 		}
 	} while(op!=27);
 }
@@ -92,31 +140,34 @@ void AbrirArquivo (char arquivo[100], List **L) {
 
 		createListOfLines(arq, &(*L));
 
-		LimpaTela();
-		gotoxy(34,6);
-		textcolor(12);
-		printf("%s",arquivo);
-		textcolor(15);
-		ExibirTexto((char*)"CONTEUDO DO ARQUIVO:",34,8,14);
+		if(!L)
+			EscrMsg("ERRO");
+		else {
+			LimpaTela();
+			gotoxy(34,6);
+			textcolor(12);
+			printf("%s",arquivo);
+			textcolor(15);
+			ExibirTexto((char*)"CONTEUDO DO ARQUIVO:",34,8,14);
 
-		ConteudoArquivo(arq);
+			ConteudoArquivo(arq);
 
-		LimpaMsg();
-		EscrMsg((char*)"PRESSIONE [F8] PARA EXECUTAR PASSO A PASSO OU [ESC] PARA VOLTAR");
+			LimpaMsg();
+			EscrMsg((char*)"PRESSIONE [F8] PARA EXECUTAR PASSO A PASSO OU [ESC] PARA VOLTAR");
 
-		do {
-			op = getch();
-			if(op == 0) {
+			do {
 				op = getch();
-				if(op == 66) {
-					ExecPassos((*L),arq); // Executa passo a passo do programa!
-					LimpaMsg();
-					LimpaTelaInteira();
-					EscrMsg("EXECUCAO FINALIZADA, PRESSIONE [ESC] PARA SAIR");
+				if(op == 0) {
+					op = getch();
+					if(op == 66) {
+						ExecPassos((*L),arq); // Executa passo a passo do programa
+						LimpaMsg();
+						EscrMsg("EXECUCAO FINALIZADA PRESSIONE [F8] PARA EXECUTAR NOVAMENTE OU [ESC] PARA SAIR");
+					}
 				}
-			}
-		} while(op != 27);
-		fclose(arq);
+			} while(op != 27);
+			fclose(arq);
+		}
 	}
 }
 
--- teste.py
+++ teste.py
@@ -1,9 +1,9 @@
 def calcula(x ,y):
    if(ola==oi):
       return true
-   else
+   else:
       return false
 
 c = calcula(5,2)
 nome='c'
-nome2="igor guilherme"             
+nome2="igor guilherme"
